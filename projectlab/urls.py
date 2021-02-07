d1676e0cde0767843d6dd1f80952b4c832e7dc20
chrisgolds <chris.golds@hotmail.com>
2021-02-07 14:04:06 +0000

    Several changes. Created and working on views for creating project, major change to account creation - university instituion determined by email domain using "universities" python package

--- projectlab/urls.py
+++ projectlab/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('verify_login/', views.verify_login, name='verify_login'),
     path('verify_sign_up/', views.verify_sign_up, name='verify_sign_up'),
-    path('<str:acc>/home/', views.home, name='home')
+    path('<str:acc>/home/', views.home, name='home'),
+    path('<str:acc>/create_project/', views.create_project, name='create_project')
 ]
--- projectlab/views.py
+++ projectlab/views.py
@@ -8,41 +8,26 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import check_password
 import os
+import re
 import datetime
+import universities
 
 from .models import Member, Workspace, Project
 
 def login(request):
-	if request.user.is_authenticated:
-		return HttpResponseRedirect(reverse('projectlab:home', args=(request.user.username,)))
-	else:
-		unis = []
-		file = open(os.path.join(os.path.dirname(__file__), 'uni.txt'))
-		for line in file:
-			unis.append(line.split("\t")[2])
-		return render(request, 'projectlab/login.html', {'unis' : unis})
+	return render(request, 'projectlab/login.html')
 
 def verify_login(request):
 	try:
 		account = Member.objects.get(usr = User.objects.get(username = request.POST['username']))
 	except ObjectDoesNotExist:
-		unis = []
-		file = open(os.path.join(os.path.dirname(__file__), 'uni.txt'))
-		for line in file:
-			unis.append(line.split("\t")[2])
 		return render(request, 'projectlab/login.html', {
-				'error_with_sign_in' : True,
-				'unis' : unis
+				'error_with_sign_in' : True
 			})
 	else:
 		if not check_password(request.POST['password'], account.usr.password):
-			unis = []
-			file = open(os.path.join(os.path.dirname(__file__), 'uni.txt'))
-			for line in file:
-				unis.append(line.split("\t")[2])
 			return render(request, 'projectlab/login.html', {
-					'error_with_sign_in' : True,
-					'unis' : unis
+					'error_with_sign_in' : True
 				})
 		user = authenticate(username=account.usr.username, password=request.POST['password'])
 		if user is not None:
@@ -57,18 +42,32 @@ def verify_sign_up(request):
 		try:
 			username = Member.objects.get(usr = User.objects.get(username = request.POST['signup_username']))
 		except ObjectDoesNotExist:
-			Member(
-				usr = User.objects.create_user(
-						username = request.POST['signup_username'],
-						password = request.POST['signup_password'],
-						email = request.POST['email'],
-						first_name = request.POST['firstName'],
-						last_name = request.POST['lastName']
-					),
-				dob = datetime.datetime.strptime(request.POST['dob'], "%d/%m/%Y").strftime("%Y-%m-%d"),
-				university = request.POST['university']
-			).save()
-			return HttpResponse("<strong>Sign up successful!<strong>")
+
+			try:
+				email = Member.objects.get(usr = User.objects.get(email = request.POST['email']))
+			except ObjectDoesNotExist:
+				addr = request.POST['email'].split('@')[1]
+				domain = re.search("\w+.ac.uk$", addr)
+				if not domain:
+					return HttpResponseServerError("<strong>The email address you have provided does not belong to a valid institution<strong>")
+				uni = list(universities.API().search(domain = domain.group()))
+				if not uni:
+					return HttpResponseServerError("<strong>The email address you have provided does not belong to a valid institution<strong>")
+				else:
+					Member(
+						usr = User.objects.create_user(
+								username = request.POST['signup_username'],
+								password = request.POST['signup_password'],
+								email = request.POST['email'],
+								first_name = request.POST['firstName'],
+								last_name = request.POST['lastName']
+						),
+						dob = datetime.datetime.strptime(request.POST['dob'], "%d/%m/%Y").strftime("%Y-%m-%d"),
+						university = uni[0].name
+					).save()
+					return HttpResponse("<strong>Sign up successful!<strong>")
+			else:
+				return HttpResponseServerError("<strong>The email address you have provided is already in use. Please try another address<strong>")
 		else:
 			return HttpResponseServerError("<strong>The username you have provided is already in use. Please try another name<strong>")
 
@@ -79,3 +78,15 @@ def home(request, acc):
 		user = Member.objects.get(usr = User.objects.get(username = request.user.username))
 		return render(request, 'projectlab/home.html', {'user' : user,
 			'projects' : user.project_set.all()})
+	else:
+		return HttpResponseRedirect(reverse('projectlab:login'))
+
+
+@login_required
+def create_project(request, acc):
+	if acc == request.user.username:
+		print("Here")
+		#TODO - Form for creating new project
+		
+	else:
+		return HttpResponseRedirect(reverse('projectlab:login'))
