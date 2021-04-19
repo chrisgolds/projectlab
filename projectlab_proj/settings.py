5fcac805e4059841d5c1b9df892f79a16aa9877f
chrisgolds <chris.golds@hotmail.com>
2021-04-19 22:04:23 +0100

    Postgres change did not work. Switching back to SQLite and will remain in contact with IT systems at OpenShift

--- projectlab_proj/settings.py
+++ projectlab_proj/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'projectlab_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': BASE_DIR / 'db',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
