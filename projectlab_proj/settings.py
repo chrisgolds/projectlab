541b808b62a005f5a6ea45b9ab2e7f41e16eb51b
chrisgolds <chris.golds@hotmail.com>
2021-04-19 17:45:57 +0100

    Invalid character in settings caused build fail

--- projectlab_proj/settings.py
+++ projectlab_proj/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
